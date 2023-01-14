7a76709fdc71cbdfb6d8500bf544c41919936ada
Emisaka <74747045+Emisaka123@users.noreply.github.com>
2023-01-14 16:17:20 +0800

    first commit

--- demo10.go
+++ demo10.go
@@ -1,2 +1,22 @@
 package main
 
+import "fmt"
+
+func main() {
+	nums := []int{2, 2, 3, 4}
+	sum := 0
+	for i, num := range nums {
+		sum += num
+		if num == 2 {
+			fmt.Println("index:", i, "num:", num)
+		}
+	}
+	fmt.Println("sum:", sum)
+	m := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	for k := range m {
+		fmt.Println("key", k)
+	}
+}
